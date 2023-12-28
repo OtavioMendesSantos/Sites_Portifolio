fd9a5356b232cfa43c96392302acea2e65a2d17b
OtavioMendesSantos <145459372+OtavioMendesSantos@users.noreply.github.com>
2023-12-28 15:23:07 -0300

    Implementando Header Com JavaScript

---
+++ assets/script/headerDinamico.js
@@ -0,0 +1,15 @@
+const header = document.getElementById("headerPoweredByJs")
+header.innerHTML += ("<h1><a href='../index.html'>Meu site</a></h1> <nav id='menu_cabecalho'> <ul> <li><a href='./formacao.html' rel='next' class='link_menu'>Quem sou eu</a></li> <li><a href='./projetos.html'  rel='next' class='link_menu'>Projetos</a></li> <li><a href='./hobbies.html' rel='next'  class='link_menu'>Hobbies</a></li> <li><a href='./tagshtml.html' rel='next' class='link_menu'>Tags Html*</a></li> <li><a href='./javascript.html'  rel='next' class='link_menu'>JavaScript*</a></li> </ul> </nav>")
+
+/* 
+<h1><a href='../index.html'>Meu site</a></h1>
+<nav id='menu_cabecalho'>
+    <ul> 
+        <li><a href='./formacao.html' rel='next' class='link_menu'>Quem sou eu</a></li>
+        <li><a href='./projetos.html'  rel='next' class='link_menu'>Projetos</a></li>
+        <li><a href='./hobbies.html' rel='next'  class='link_menu'>Hobbies</a></li>
+        <li><a href='./tagshtml.html' rel='next' class='link_menu'>Tags Html*</a></li>
+        <li><a href='./javascript.html'  rel='next' class='link_menu'>JavaScript*</a></li>
+    </ul>
+</nav>
+*/
