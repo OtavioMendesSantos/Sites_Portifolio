4e2047bee46a28fae5369b1969de90324ce646c9
OtavioMendesSantos <145459372+OtavioMendesSantos@users.noreply.github.com>
2024-02-06 15:26:11 -0300

    Adicionando Carrossel e Certificados

--- assets/scripts/formacao.js
+++ assets/scripts/formacao.js
@@ -14,3 +14,46 @@ function initHideList(){
     }) 
 }
 initHideList()
+
+
+/* Carrossel / Carousel */
+let slideIndex = 1;
+let slideInterval;
+const slides = document.querySelectorAll('.certificates')
+const dots = document.querySelectorAll('.dot')
+
+showSlides(slideIndex);
+
+function plusSlides(n){
+    showSlides(slideIndex += n)
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function numberText(){
+    slides[slideIndex - 1].querySelector('.number-text').innerHTML = `${slideIndex} / ${slides.length}`
+}
+
+function showSlides(n){
+    n > slides.length ? slideIndex = 1 : '';
+    n < 1 ? slideIndex = slides.length : '';
+    slides.forEach((slide)=>{
+        slide.style.display = 'none'
+    })
+    dots.forEach((dot)=>{
+        dot.classList.remove('carousel-active')
+    })
+    slides[slideIndex - 1].style.display = 'block';
+    dots[slideIndex - 1].classList.add('carousel-active');
+
+    numberText()
+    
+    clearTimeout(slideInterval)
+
+    slideInterval = setTimeout(()=>{
+        slideIndex++
+        showSlides(slideIndex)
+    }, 5000)
+}
