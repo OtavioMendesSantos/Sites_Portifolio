a622d47289683c2104ab819c16b47f99ebc6c840
OtavioMendesSantos <145459372+OtavioMendesSantos@users.noreply.github.com>
2024-02-08 11:25:25 -0300

    Projeto Simples Relógio

---
+++ projetos/autoritarios/Relogio_Digital/assets/scripts/engine.js
@@ -0,0 +1,11 @@
+const timer = document.querySelector("#timer");
+let hours, minutes, seconds, now;
+
+let updateClock = setInterval(()=>{
+    now = new Date;
+    hours = (now.getHours()).toString().padStart(2, '0');
+    minutes = (now.getMinutes()).toString().padStart(2, '0');
+    seconds = (now.getSeconds()).toString().padStart(2, '0');
+    
+    timer.innerHTML = `${hours}:${minutes}:${seconds}`;
+},1000) 
