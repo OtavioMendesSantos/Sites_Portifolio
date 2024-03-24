076295c26c681f6fff5b74d438ee1564741c5133
OtavioMendesSantos <145459372+OtavioMendesSantos@users.noreply.github.com>
2024-03-23 22:29:27 -0300

    :bug: fix: Menu Mobile

--- projetos/origamid/Animais_Fantasticos/assets/scripts/modules/menu-mobile.js
+++ projetos/origamid/Animais_Fantasticos/assets/scripts/modules/menu-mobile.js
@@ -3,14 +3,14 @@ import outsideClick from "./oustideclick.js";
 export default function initMenuMobile(){
     const menuButton = document.querySelector('[data-menu="button"]')
     const menuList = document.querySelector('[data-menu="list"]')
-    const eventos = ['click', 'touchstart']
+    const eventos = ['click'] //['touchstart'] -> Não funciona correto
 
     if(!menuButton) return
 
     function openMenu(event){
         menuList.classList.add('active')
         menuButton.classList.add('active')
-        outsideClick(menuList, ['click', 'touchstart'], ()=>{
+        outsideClick(menuList, eventos, ()=>{
             menuList.classList.remove('active')
             menuButton.classList.remove('active')
         })
